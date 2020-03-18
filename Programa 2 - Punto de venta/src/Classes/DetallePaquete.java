5d463bbcf9c805b71a67537f052add569c5a91a3
McPepe26 <andyzam301@gmail.com>
2020-03-17 18:47:17 -0600

    Agregada clase DetallePaquete

---
+++ Programa 2 - Punto de venta/src/Classes/DetallePaquete.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author ja-za
+ */
+public class DetallePaquete {
+    private int clave;
+    private int clavePaquete;
+    private int productoPaquete;
+    private int cantidad;
+
+    public DetallePaquete(int clave, int clavePaquete, int productoPaquete, int cantidad) {
+        this.clave = clave;
+        this.clavePaquete = clavePaquete;
+        this.productoPaquete = productoPaquete;
+        this.cantidad = cantidad;
+    }
+
+    public DetallePaquete(int clavePaquete, int productoPaquete, int cantidad) {
+        this.clavePaquete = clavePaquete;
+        this.productoPaquete = productoPaquete;
+        this.cantidad = cantidad;
+    }
+
+    public int getClave() {
+        return clave;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+
+    public int getClavePaquete() {
+        return clavePaquete;
+    }
+
+    public void setClavePaquete(int clavePaquete) {
+        this.clavePaquete = clavePaquete;
+    }
+
+    public int getProductoPaquete() {
+        return productoPaquete;
+    }
+
+    public void setProductoPaquete(int productoPaquete) {
+        this.productoPaquete = productoPaquete;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
